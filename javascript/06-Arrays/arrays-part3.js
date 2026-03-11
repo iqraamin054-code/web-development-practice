334724a83b1f54c64e5d38297c55986a58923247
Iqra Amin <iqraamin087@gmail.com>
2026-03-11 23:45:11 +0500

    Arrays Part 3: practiced array iteration using for...of and forEach, printing values, squares, indexes, and calculating sum

---
+++ javascript/06-Arrays/arrays-part3.js
@@ -0,0 +1,62 @@
+// Arrays Part 3 – Looping Through Arrays
+
+// 1. Create an array of numbers:[3, 6, 9, 12, 15]. Use a for...of loop to print each number.
+
+const numbers = [3, 6, 9, 12, 15];
+// This directly gives you each value in the array.
+for (let num of numbers) {
+    console.log(num);    
+}
+
+/* 2. Create an array:
+["Ali", "Sara", "Ahmed", "Zara"]
+
+Use a for...of loop to print:
+Hello Ali
+Hello Sara
+Hello Ahmed
+Hello Zara */
+
+const myArray = ["Ali", "Sara", "Ahmed", "Zara"];
+
+for(let arr of myArray){
+    console.log(`Hello ${arr}`);
+}
+
+
+// 3. Create an array of numbers:[2, 4, 6, 8, 10]. Use forEach() to print the square of each number.
+
+const myNumbers = [2, 4, 6, 8, 10];
+
+myNumbers.forEach((num) => { // callback function
+    console.log(num*num);
+});
+
+
+/* 4. Create an array:
+["apple", "banana", "mango"]
+
+Use forEach() to print:
+0 apple
+1 banana
+2 mango
+
+(index and value) */
+
+const anArray = ["apple", "banana", "mango"];
+
+anArray.forEach((value, index) => { // value current element,index the position of that element
+    console.log(`Index: ${index} and Value: ${value}`);
+});
+
+
+// 5. Create an array:[5, 10, 15, 20]. Use a loop to calculate the total sum and print it.
+
+let sum = 0;
+const num = [5, 10, 15, 20];
+for(let arr of num){
+    sum += arr;
+    console.log(arr);
+}
+console.log(sum); 
+
