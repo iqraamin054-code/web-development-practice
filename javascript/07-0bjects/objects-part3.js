0d39f0e9eedeefa11125acbd98bf1af51ac161a2
Iqra Amin <iqraamin087@gmail.com>
2026-03-15 01:58:11 +0500

    Objects Part 3: Object.keys, Object.values, entries, for...in loop, delete property

---
+++ javascript/07-0bjects/objects-part3.js
@@ -0,0 +1,77 @@
+// Objects Part 3 
+
+/* Task 1 Create an object called car with these properties:
+brand: "Toyota"
+model: "Corolla"
+year: 2022
+Print all keys of the object */
+
+
+const car = {
+  brand: "Toyota",
+    model: "Corolla",
+    year: 2022    
+}
+
+console.log(Object.keys(car));
+
+
+/* Task 2 Create an object called laptop with these properties:
+brand: "Dell"
+ram: "16GB"
+storage: "512GB"
+Print all values of the object */
+
+const laptop = {
+  brand: "Dell",
+    ram: "16GB",
+    storage: "512GB"  
+}
+
+console.log(Object.values(laptop));
+
+
+/* Task 3 Create an object called book with these properties:
+title: "Atomic Habits"
+author: "James Clear"
+pages: 320
+Print both keys and values */
+
+const book = {
+   title: "Atomic Habits",
+    author: "James Clear",
+    pages: 320 
+}
+
+console.log(Object.entries(book));
+
+
+/* Task 4 Create an object called student:
+name: "Sara"
+age: 20
+course: "Computer Science"
+Use a for...in loop to print all properties.  */
+
+const student = {
+    name: "Iqra",
+    age: 20,
+    course: "Software Enginnering"
+}
+
+for(let items in student){
+    console.log(`keys: ${items} and values: ${student[items]}`);
+}
+
+
+/* Task 5 Create an object called product:
+name: "Keyboard"
+price: 50
+Delete the price property and print the object. */
+
+const product = {
+    name: "Keyboard",
+    price: 50
+}
+
+delete product.price;
+console.log(product);
